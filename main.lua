a4fd0395bde550adbbbf99de1cae2771be958930
smonaghan <smonaghan@eitccorp.com>
2020-02-16 22:16:06 -0500

    character fun with anim8

--- main.lua
+++ main.lua
@@ -1,19 +1,35 @@
---[[
-   main.lua
-   The main entry point into the game
-]]--
+local tiny = require 'lib.tiny'
+local anim8 = require 'lib.anim8'
+
+require 'entity'
 
 local util = require 'util'
 
 local images = {}
 
 function love.load()
+   walkingImg = love.graphics.newImage('img/walking.png')
+   idleImg = love.graphics.newImage('img/idle.png')
+   jumpImg = love.graphics.newImage('img/jump.png')
+   loopingGrid = anim8.newGrid(32, 32, 128, 32, 0, 0, 0)
+   loopingAnim = anim8.newAnimation(loopingGrid('1-4', 1), 0.1)
+   jumpingGrid = anim8.newGrid(32, 32, 256, 32, 0, 0, 0)
+   jumpingAnim = anim8.newAnimation(jumpingGrid('1-8', 1), 0.1)   
 end
 
 function love.update(dt)
+   loopingAnim:update(dt)
+   jumpingAnim:update(dt)
 end
 
 function love.draw()
+   red = 97/255
+   green = 85/255
+   blue = 97/255
+   love.graphics.setBackgroundColor({red,green,blue})
+   loopingAnim:draw(walkingImg, 100, 100)
+   loopingAnim:draw(idleImg, 150, 100)
+   jumpingAnim:draw(jumpImg, 200, 100)
    for k,v in pairs(images) do
       love.graphics.draw(v, util.center(v))
    end   
