22c9631efb10344fa253f789e4092db77f2eb982
smonaghan <smonaghan@eitccorp.com>
2020-02-22 01:03:47 -0500

    larger sprites, fast modee

--- production/dark_earth.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="dark_earth_tmp" tilewidth="16" tileheight="16" tilecount="7" columns="7">
- <image source="dark_earth_tmp.png" width="112" height="16"/>
-</tileset>
